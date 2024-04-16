0e37021ccfe88480990b59c43a47cb3c548b7595
Nikhil Soni <nikhilbhoma@gmail.com>
2024-04-16 20:40:56 +0530

    Laying the structure, adding modules & components, defining routes, implemented lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HeaderComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'events',
+        loadChildren: () => import('./modules/events/events.module').then(m => m.EventsModule)
+      },
+      {
+        path: 'videos',
+        loadChildren: () => import('./modules/videos/videos.module').then(m => m.VideosModule)
+      },
+      {
+        path: 'live',
+        loadChildren: () => import('./modules/live/live.module').then(m => m.LiveModule)
+      },
+      {
+        path: 'register',
+        loadChildren: () => import('./modules/register/register.module').then(m => m.RegisterModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
